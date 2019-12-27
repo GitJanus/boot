9b4ab07758c154d24a23c56db9c598573815a2ee
janus <janus.zou@outlook.com>
2019-12-27 15:47:26 +0800

    双向ssl认证

--- src/main/java/com/example/boot/BootApplication.java
+++ src/main/java/com/example/boot/BootApplication.java
@@ -1,12 +1,25 @@
 package com.example.boot;
 
+import com.example.boot.filter.BaseFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BootApplication.class, args);
+    }
+
+    //添加一个基本过滤器，用于设置编码，调试观察请求数据
+    @Bean
+    public FilterRegistrationBean registerBaseFilter() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(new BaseFilter());
+        registration.addUrlPatterns("/*");
+        registration.setOrder(1);
+        return registration;
     }
 }
---
+++ src/main/java/com/example/boot/filter/BaseFilter.java
@@ -0,0 +1,27 @@
+package com.example.boot.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Array;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class BaseFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) {
+       System.out.println("filter start...");
+       HttpServletRequest req = (HttpServletRequest) servletRequest;
+        X509Certificate[] certs = (X509Certificate[]) req.getAttribute("javax.servlet.request.X509Certificate");
+        System.out.println(Arrays.toString(certs));
+    }
+
+    @Override
+    public void init(FilterConfig config) {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
